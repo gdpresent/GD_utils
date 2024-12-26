d28cf32d8082bf8d84ea3e8b8ab6c515810caf1d
gd_park <presentgdpark@gmail.com>
2024-12-27 08:26:20 +0900

    gd

--- GD_utils/__init__.py
+++ GD_utils/__init__.py
@@ -1,4 +1,4 @@
-from GD_utils import get_data, portfolio_calculator
+from GD_utils import get_data, portfolio_calculator, general_utils
 from GD_utils.Report import *
 from GD_utils.calc_return import *
 data=None
